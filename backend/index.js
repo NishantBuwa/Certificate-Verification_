4407184d332213d31e075177f9842cac6d7f44e1
Nishant Buwa <nishantbuwa01@gmail.com>
2025-08-06 21:19:39 +0530

    CORS Fixed

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
