f28251496cd8518af7d4fceed83ef83b20dd7678
Nishant Buwa <nishantbuwa01@gmail.com>
2025-08-09 00:25:59 +0530

    Final Changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
     useEffect(() => {
         const startServer = async () => {
             try {
-                const data = await fetch(`${REACT_APP_API_URL}/`);
+                const data = await fetch(`${process.env.REACT_APP_API_URL}/`);
                 const res = await data.json();
                 if (res) {
                     // toast.success('Server Started');
